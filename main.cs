3dda70ade96f9085c4049acc113577085ad5254a
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-10-11 18:39:07 +0000

    added comment

--- main.cs
+++ main.cs
@@ -1,8 +1,13 @@
+// Created by: Alex Nelson
+// Created on: Oct 2022
+//
+// This program does basic math
 using System;
 
 class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("The dimesions of a rectangle are 3 cm and 5 cm");
-    Console.WriteLine ("The area is: " + (5 * 3));
+    Console.WriteLine ("The area is: " + (5 * 3) + " cm²");
+    Console.WriteLine ("The perimeter is: " + (2 * (5 + 3)) + "cm");
   }
 }
