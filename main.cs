4830f1c002284fab9fbdc5958af5c5bc60720354
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-10-12 19:18:01 -0400

    Update main.cs

--- main.cs
+++ main.cs
@@ -4,7 +4,9 @@
 // This program does basic math
 using System;
 
-class Program{public static void Main(string[] args){Console.WriteLine("The dimensions of a rectangle are 3 cm and 5 cm");
+class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("The dimensions of a rectangle are 3 cm and 5 cm");
         Console.WriteLine("The area is: " + (5 * 3) + " cm²");
         Console.WriteLine("The perimeter is: " + (2 * (5 + 3)) + "cm");
     }
